e71b537ecbbb2c5665ac9fa87efcab55672a191a
Javapisk <giagea26@gmail.com>
2023-01-26 23:55:05 -0300

    test 1

---
+++ Zadanie 1/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int b = 6;
+        int c = 11;
+        int a = 5;
+        System.out.println("Сложение: " + (a + b + c));
+        float e = a - b - c;
+        System.out.println("Вычитание: " + e);
+        System.out.println("Умножение: " + (a * b * c));
+        System.out.println("Деление: " + (a / b /c));
+//        DecimalFormat df = new DecimalFormat(55,455555);
+    }
+}
